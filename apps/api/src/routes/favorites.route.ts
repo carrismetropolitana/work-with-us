ed58772cd82174edd572443ef3b9ff3a922b3acc
lucaslimabica <delimabica@gmail.com>
2026-04-09 23:16:34 +0100

    feat: Implement favorites API GET routes. Retrieving all favorites and a specific favorite by lineId

--- apps/api/src/routes/favorites.route.ts
+++ apps/api/src/routes/favorites.route.ts
@@ -1,4 +1,21 @@
 import { FastifyInstance } from 'fastify'
 import { Favorite } from '../models/favorite.model'
 
-export async function favoritesRoutes(app: FastifyInstance) {} //TODO CREATE ROUTES
+export async function favoritesRoutes(app: FastifyInstance) {
+    // GET all favorites
+    app.get('/favorites', async (request, reply) => {
+        const favorites = await Favorite.find()
+        reply.send(favorites)
+    })
+    // GET a specific one
+    app.get('/favorites/:lineId', async (request, reply) => {
+        const { lineId } = request.params as { lineId: string }
+        const favorite = await Favorite.findOne({ lineId })
+
+        if (!favorite) {
+            reply.status(404).send({ message: 'Favorite not found or do not exist' })
+        }
+
+        reply.send(favorite)
+    })
+} 
