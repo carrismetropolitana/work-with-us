e92ff9e7ebf1b4fbdd5926c8e8a6bff65ecb8386
lucaslimabica <delimabica@gmail.com>
2026-04-10 05:32:12 +0100

    feat: replace Button component with LineCard for enhanced line details display.
    todo: Create the line object to cronstruct the page

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-import { Button } from '@tmlmobilidade/ui';
+// import { Button } from '@tmlmobilidade/ui';
+// 
+// export default function Page() {
+// 	return (
+// 		<div>
+// 			<Button label="Click Me" onClick={() => alert('Button Clicked!')} />
+// 			<h1>Welcome to TML Frontend</h1>
+// 		</div>
+// 	);
+// }
+// 
+
+import LineCard from '../components/LineCard'
 
 export default function Page() {
 	return (
-		<div>
-			<Button label="Click Me" onClick={() => alert('Button Clicked!')} />
-			<h1>Welcome to TML Frontend</h1>
+		<div style={{ padding: '40px', 'maxWidth': '700px' }}>
+			<LineCard line={{line}} isFavorite={true} onToggleFavorite={(lineId) => console.log('Toggled favorite for line:', lineId)} />
 		</div>
-	);
-}
+	)
+}	
