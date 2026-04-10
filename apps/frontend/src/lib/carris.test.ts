4109e50238f53231b83538e333846bf9f578f503
lucaslimabica <delimabica@gmail.com>
2026-04-10 04:27:04 +0100

    chore: A class for Carris Stops, Routes and another to aggregate both within a CarrisLine were created.
    style: format code to tab 4 (only in carris.ts files) for consistency in carris.ts and carris.test.ts

--- apps/frontend/src/lib/carris.test.ts
+++ apps/frontend/src/lib/carris.test.ts
@@ -1,12 +1,12 @@
 import { getCarrisLines } from './carris'
 
 async function testGetCarrisLines() {
-  try {
-    const lines = await getCarrisLines()
-    console.log('Carris Lines:', lines)
-  } catch (error) {
-    console.error('Error fetching Carris lines:', error)
-  }
+    try {
+        const lines = await getCarrisLines()
+        console.log('Carris Lines:', lines)
+    } catch (error) {
+        console.error('Error fetching Carris lines:', error)
+    }
 }
 
 testGetCarrisLines()
--- apps/frontend/src/lib/carris.ts
+++ apps/frontend/src/lib/carris.ts
@@ -1,22 +1,46 @@
 const CARRIS_API = process.env.CARRIS_PUBLIC_API_URL || 'https://api.carrismetropolitana.pt'
 
 export interface CarrisLine {
-  id: string
-  short_name: string
-  long_name: string
-  color: string
-  text_color: string
-  route_ids: string[]
-  stop_ids: string[]
-  tts_name: string
+    id: string
+    short_name: string
+    long_name: string
+    color: string
+    text_color: string
+    route_ids: string[]
+    stop_ids: string[]
+    tts_name: string
+}
+
+// The API returns the routes and stops as a full array,
+// so map/filter will run for each line to get its routes and stops.
+// This is not ideal, but it is the only way
+export interface CarrisRoute {
+    id: string
+    line_id: string
+    long_name: string
+    tts_name: string
+    color: string
+    text_color: string
+}
+
+export interface CarrisStop {
+    id: string
+    long_name: string
+    lat: number
+    lon: number
+}
+
+export interface ErinchedLine extends CarrisLine {
+    routes: CarrisRoute[]
+    stops: CarrisStop[]
 }
 
 export async function getCarrisLines(): Promise<CarrisLine[]> {
-  const response = await fetch(`${CARRIS_API}/v2/lines`)
+    const response = await fetch(`${CARRIS_API}/v2/lines`)
   
-  if (!response.ok) {
-    throw new Error('Failed to fetch lines')
-  }
+    if (!response.ok) {
+        throw new Error('Failed to fetch lines')
+    }
 
-  return response.json()
+    return response.json()
 }
