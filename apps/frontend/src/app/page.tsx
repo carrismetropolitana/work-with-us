6d69aebe0b8bb35468353039816507c14e452a6c
lucaslimabica <delimabica@gmail.com>
2026-04-10 16:29:10 +0100

    feat: refactor Page component to use LinesGrid and with buttons for filter at top

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,30 +1,19 @@
-import LineCard from '../components/LineCard'
-import { getEnrichedLines , EnrichedLine } from '../lib/carris'
-
-// Setting up each content of the page
-interface LinesGridProps {
-  lines: EnrichedLine[]
-  initialFavorites: string[]
-}
+import LinesGrid from '../components/LinesGrid'
+import { getEnrichedLines } from '../lib/carris'
+import { getFavorites } from '../lib/favorite'
 
 export default async function Page() {
-	const lines = await getEnrichedLines()
-	return (
-		<div style={{ padding: '40px', 'maxWidth': '1000px', margin: '0 auto' }}>
-			<h1>Minha Carris Metropolitana</h1>
-			
-			{/* List of lines */}
-			<div style={{
-				display: 'grid',
-				marginTop: '24px',
-				gap: '16px',
-				gridTemplateColumns: 'repeat(auto-fill, minmax(600px, 1fr))'
-			}}> { /* A map for each line from the enriched lines getter */ } 
-				{lines.map(line => (
-					<LineCard key={line.id} line={line} isFavorite={false} />
-				))}
-			</div>
+	const [lines, favorites] = await Promise.all([
+    	getEnrichedLines(),
+    	getFavorites(),
+  	])
 
-		</div>
-	)
-}	
+	const favoriteIds = favorites.map((f: { lineId: string }) => f.lineId)
+
+  	return (
+    	<div style={{ maxWidth: '1000px', margin: '0 auto', padding: '40px' }}>
+      		<h1>Minha Carris Metropolitana</h1>
+      		<LinesGrid lines={lines} initialFavorites={favoriteIds} />
+    	</div>
+  	)
+}
