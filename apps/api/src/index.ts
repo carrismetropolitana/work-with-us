8d7534df270598d0c91c777ce119193a69dd0c3d
lucaslimabica <delimabica@gmail.com>
2026-04-10 16:54:08 +0100

    feat: add CORS support to enable frontend CRUD operations

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,7 @@
 import Fastify from 'fastify'
 import mongoose from 'mongoose'
 import { favoritesRoutes } from './routes/favorites.routes'
+import cors from '@fastify/cors'
 
 /* * */
 
@@ -20,11 +21,15 @@ const app = Fastify()
 
 // Connect to MongoDB and start the server
 mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
+  .then(async () => {
     console.log('Debug: Connected to MongoDB') // Debug log for successful connection
 
     app.register(favoritesRoutes) // Connect API routes
     console.log('Debug: API routes registered') // Debug log for successful route registration
+
+    await app.register(cors, { // Enable the funcction of CRUD by the frontend
+      origin: '*'
+    })
 
     app.listen({ port: Number(process.env.PORT) }, (err) => {
       if (err) throw err
